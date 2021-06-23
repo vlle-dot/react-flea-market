56489dab5b9baed6b22bd4dbf914ad42d2b31745
vlle-dot <73817969+vlle-dot@users.noreply.github.com>
2021-06-23 15:21:40 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,16 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
 import toriData from "./data/kirpputorit.json";
-import teslaData from "./data/tesla-sites.json";
 /*
-use  "import toriData" same way as "import teslaData"
+
 */
 
 function App() {
 
-  const filteredStations = teslaData.filter(tsla => tsla.address.country === "Italy")
   const torit = toriData
     
   return (
-      <MapContainer center={[42.585444, 13.257684]} zoom={4} scrollWheelZoom={true}>
+      <MapContainer center={[60.169857, 24.938379]} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -24,19 +22,8 @@ function App() {
           <Marker key = {tori.id} position={[tori.gps.latitude, tori.gps.longitude]}>
             <Popup position={[tori.gps.latitude, tori.gps.longitude]}>
               <div>
-                <h3>{"Name: " + tori.name}</h3>
-                <p>{"Status: " + tori.address.street}</p>
-              </div>
-            </Popup>
-          </Marker>
-        ))};
-        
-        {filteredStations.map(tsla => (
-          <Marker key = {tsla.id} position={[tsla.gps.latitude, tsla.gps.longitude]}>
-            <Popup position={[tsla.gps.latitude, tsla.gps.longitude]}>
-              <div>
-                <h3>{"Name: " + tsla.name}</h3>
-                <p>{"Street: " + tsla.address.street}</p>
+                <h3>{"Nimi: " + tori.name}</h3>
+                <p>{"Osoite: " + tori.address.street}</p>
               </div>
             </Popup>
           </Marker>
